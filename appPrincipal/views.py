899402e79eeb25a3e4d1b4d4dacb44998f41bf7c
santiago <sochoa16.finca@gmail.com>
2021-11-04 20:59:30 -0500

    Se elimino app principal

--- appPrincipal/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render, HttpResponse
-#Importar HttpResponse
-
-# Create your views here.
-
-#Inicio
-def home(request):
-
-    return render(request, "appPrincipal/home.html")
